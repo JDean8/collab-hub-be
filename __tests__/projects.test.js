902b5f2d9bb6ceb715499a2237ddbfab523952f2
Sir1ys <sashakoroviy@gmail.com>
2023-12-13 09:53:17 +0000

    first test for posting a new project is written

--- __tests__/projects.test.js
+++ __tests__/projects.test.js
@@ -29,3 +29,32 @@ describe("GET /api/projects", () => {
       });
   });
 });
+
+escribe("POST /api/projects", () => {
+  test("201: responds with a project object", () => {
+    return request(app)
+      .post("/api/projects")
+      .send({
+        project: {
+          project_author: 2,
+          project_name: "Collab Hub",
+          project_description: "Interesting project for finding team members",
+          project_created_at: 1669852800000,
+          required_members: 3,
+        },
+      })
+      .expect(201)
+      .then(({ body: { project } }) => {
+        expect(project).toEqual(
+          expect.objectContaining({
+            project_id: expect.any(Number),
+            project_author: expect.any(Number),
+            project_name: expect.any(String),
+            project_description: expect.any(String),
+            project_created_at: expect.any(String),
+            required_members: expect.any(Number),
+          })
+        );
+      });
+  });
+});
