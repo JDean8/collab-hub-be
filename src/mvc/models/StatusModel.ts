75524388c0c0396e7cc81c47013c6e41f1bc861e
karolinakonior <karolina.koniorr@gmail.com>
2023-12-13 11:15:35 +0000

    Completed GET status

---
+++ src/mvc/models/StatusModel.ts
@@ -0,0 +1,13 @@
+import { type Status } from "../../db/data/test-data/status";
+const db = require("../../../dist/db/pool.js");
+
+type StatusProps = {
+    rows: Status[]
+}
+
+exports.fetchStatus = () => {
+    return db.query("SELECT * FROM status")
+    .then(({rows}: StatusProps) => {
+        return rows;
+    })
+}
