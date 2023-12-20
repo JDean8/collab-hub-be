f893a826e3e18fc69a8e1091a506538ee1be0442
Sir1ys <sashakoroviy@gmail.com>
2023-12-20 10:38:45 +0000

    app was destructed

--- listen.js
+++ listen.js
@@ -1,4 +1,4 @@
-const app = require("./dist/api.js");
+const { app } = require("./dist/api.js");
 const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
