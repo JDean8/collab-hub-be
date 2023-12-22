b057ae82711bd8b74443a7fe1962476be1f34a11
karolinakonior <karolina.koniorr@gmail.com>
2023-12-22 11:36:35 +0000

    Added more relationships to projects-skills junction table

--- src/db/data/dev-data/projects-skills.ts
+++ src/db/data/dev-data/projects-skills.ts
@@ -13,12 +13,68 @@ export const projects_skills: Project_skill[] = [
     skill_id: 2,
   },
   {
+    project_id: 1,
+    skill_id: 3,
+  },
+  {
+    project_id: 1,
+    skill_id: 4,
+  },
+  {
+    project_id: 1,
+    skill_id: 5,
+  },
+  {
     project_id: 2,
     skill_id: 3,
   },
   {
+    project_id: 2,
+    skill_id: 4,
+  },
+  {
+    project_id: 2,
+    skill_id: 16,
+  },
+  {
+    project_id: 2,
+    skill_id: 19,
+  },
+  {
+    project_id: 2,
+    skill_id: 7,
+  },
+  {
+    project_id: 2,
+    skill_id: 6,
+  },
+  {
     project_id: 3,
     skill_id: 4,
+  },
+  {
+    project_id: 3,
+    skill_id: 5,
+  },
+  {
+    project_id: 3,
+    skill_id: 4,
+  },
+  {
+    project_id: 3,
+    skill_id: 16,
+  },
+  {
+    project_id: 3,
+    skill_id: 19,
+  },
+  {
+    project_id: 4,
+    skill_id: 5,
+  },
+  {
+    project_id: 4,
+    skill_id: 10,
   },
   {
     project_id: 4,
@@ -26,7 +82,23 @@ export const projects_skills: Project_skill[] = [
   },
   {
     project_id: 5,
-    skill_id: 6,
+    skill_id: 12,
+  },
+  {
+    project_id: 4,
+    skill_id: 19,
+  },
+  {
+    project_id: 6,
+    skill_id: 1,
+  },
+  {
+    project_id: 6,
+    skill_id: 20,
+  },
+  {
+    project_id: 6,
+    skill_id: 16,
   },
   {
     project_id: 6,
@@ -37,12 +109,44 @@ export const projects_skills: Project_skill[] = [
     skill_id: 1,
   },
   {
+    project_id: 7,
+    skill_id: 2,
+  },
+  {
+    project_id: 7,
+    skill_id: 3,
+  },
+  {
+    project_id: 7,
+    skill_id: 4,
+  },
+  {
+    project_id: 7,
+    skill_id: 5,
+  },
+  {
+    project_id: 7,
+    skill_id: 6,
+  },
+  {
     project_id: 8,
     skill_id: 2,
   },
   {
     project_id: 8,
+    skill_id: 3,
+  },
+  {
+    project_id: 8,
     skill_id: 4,
+  },
+  {
+    project_id: 8,
+    skill_id: 6,
+  },
+  {
+    project_id: 8,
+    skill_id: 7,
   },
   {
     project_id: 8,
@@ -53,19 +157,103 @@ export const projects_skills: Project_skill[] = [
     skill_id: 1,
   },
   {
+    project_id: 9,
+    skill_id: 2,
+  },
+  {
+    project_id: 9,
+    skill_id: 3,
+  },
+  {
+    project_id: 9,
+    skill_id: 4,
+  },
+  {
+    project_id: 9,
+    skill_id: 5,
+  },
+  {
+    project_id: 9,
+    skill_id: 6,
+  },
+  {
     project_id: 10,
     skill_id: 1,
+  },
+  {
+    project_id: 10,
+    skill_id: 20,
+  },
+  {
+    project_id: 10,
+    skill_id: 19,
+  },
+  {
+    project_id: 10,
+    skill_id: 2,
+  },
+  {
+    project_id: 10,
+    skill_id: 3,
+  },
+  {
+    project_id: 10,
+    skill_id: 4,
   },
   {
     project_id: 11,
     skill_id: 1,
   },
   {
+    project_id: 11,
+    skill_id: 8,
+  },
+  {
+    project_id: 11,
+    skill_id: 17,
+  },
+  {
+    project_id: 11,
+    skill_id: 2,
+  },
+  {
+    project_id: 11,
+    skill_id: 3,
+  },
+  {
     project_id: 12,
     skill_id: 1,
   },
   {
     project_id: 12,
+    skill_id: 2,
+  },
+  {
+    project_id: 12,
+    skill_id: 3,
+  },
+  {
+    project_id: 12,
+    skill_id: 4,
+  },
+  {
+    project_id: 12,
     skill_id: 5,
+  },
+  {
+    project_id: 12,
+    skill_id: 5,
+  },
+  {
+    project_id: 12,
+    skill_id: 9,
+  },
+  {
+    project_id: 12,
+    skill_id: 16,
+  },
+  {
+    project_id: 12,
+    skill_id: 19,
   },
 ];
