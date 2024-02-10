8252afe39daf45ac70500d2a26a1536a7d8151a2
karolinakonior <karolina.koniorr@gmail.com>
2024-02-10 11:10:43 +0000

    Updated dev data for chat members

--- dist/db/data/dev-data/chat.js
+++ dist/db/data/dev-data/chat.js
@@ -10,5 +10,32 @@ exports.chat = [
     },
     {
         chat_id: '3'
+    },
+    {
+        chat_id: '4'
+    },
+    {
+        chat_id: '5'
+    },
+    {
+        chat_id: '6'
+    },
+    {
+        chat_id: '7'
+    },
+    {
+        chat_id: '8'
+    },
+    {
+        chat_id: '9'
+    },
+    {
+        chat_id: '10'
+    },
+    {
+        chat_id: '11'
+    },
+    {
+        chat_id: '12'
     }
 ];
